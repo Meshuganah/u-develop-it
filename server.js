cf43e868d621fcef99afa48cdcaac5c5ab4392ba
Josh Klein <kleinjjosh74@gmail.com>
2022-04-30 13:18:27 -0400

    updated candidate table to show party affiliation

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ const db = mysql.createConnection(
 
 //Gets a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query (sql, params, (err, row) => {
@@ -36,6 +41,26 @@ app.get('/api/candidate/:id', (req, res) => {
         res.json({
             message: 'success',
             data: row,
+        });
+    });
+});
+
+//Get's all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
         });
     });
 });
@@ -85,36 +110,6 @@ app.post('/api/candidate', ({ body }, res) => {
         });
     });
 });
-
-//Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//             VALUES (?,?,?,?)`;
-
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
-
-//Get's all candidates
-app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
-
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
 
 //Default response for any other request (Not Found) **NEEDS TO BE LAST**
 app.use((req, res) => {
